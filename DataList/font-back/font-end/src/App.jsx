2b747c47ddd6d4bf650f7c03e4ffaca9bb751c8a
Thawitchai <124795506+THAWITCHAI@users.noreply.github.com>
2024-01-28 18:29:16 +0700

    up

--- DataList/font-back/font-end/src/App.jsx
+++ DataList/font-back/font-end/src/App.jsx
@@ -1,34 +1,42 @@
 /* eslint-disable react/jsx-no-target-blank */
-import { useState, useEffect } from 'react'
-
+import { useState, useEffect } from "react";
+import Navbar from "./Component/Navbar";
+import axios from "axios";
 import './App.css'
-import axios from 'axios'
 
 function App() {
-  const [data, setData] = useState({})
-  useEffect(()=>{
-
-    axios.get('http://localhost:3000/').then( res =>{
-      setData(res.data)
-      console.log(res)
-    }).catch(err=>{
-      console.log(err)
-    })
-
-  },[])
+  const [data, setData] = useState({});
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/")
+      .then((res) => {
+        setData(res.data);
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <>
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>{data.title}</title>
-    </head>
-      <h1>{data.title}</h1>
-      <button className='btn'><a target='_blank' href={data.link}>Facebook</a></button>
-      
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{data.title}</title>
+      </head>
+      <Navbar/>
+      <body>
+        <h1>{data.title}</h1>
+        <button className="btn">
+          <a target="_blank" href={data.link}>
+            Facebook
+          </a>
+        </button>
+
+      </body>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ DataList/font-back/font-end/src/Component/Navbar.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <span>Menu</span>
+      <span>Leading</span>
+      <span>Login</span>
+    </div>
+  )
+}
+
+export default Navbar
