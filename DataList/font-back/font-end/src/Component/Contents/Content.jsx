3d41c6dc785658e895a7e18534e4e32540b58e92
Thawitchai <124795506+THAWITCHAI@users.noreply.github.com>
2024-02-29 18:09:13 +0700

    commit

--- DataList/font-back/font-end/src/Component/Contents/Content.jsx
+++ DataList/font-back/font-end/src/Component/Contents/Content.jsx
@@ -1,6 +1,554 @@
 import "./login.css";
 
 function Content() {
+  var data = [
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+    {
+      name: "Thawitchai",
+      last: "Boonsong",
+      Email: "Thawitchai285@gmail.com",
+      Phone_Number: "0652974104",
+    },
+  ];
   return (
     <section id="contact" className="text-center">
       <h1>ข้อมูลลูกค้า</h1>
@@ -18,34 +566,41 @@ function Content() {
             </tr>
           </thead>
           <tbody>
-            <tr className="tt">
-              <th scope="row">1</th>
-              <td>นาย ธวิชชัย</td>
-              <td>บุญส่ง</td>
-              <td>Thawitchai@gmail.com</td>
-              <td>0652974104</td>
-              <td>
-                <button type="button" className="btn-sm btn btn-outline-warning">
-                  <img
-                    src="https://cdn-icons-png.flaticon.com/128/1159/1159633.png"
-                    width={24}
-                    height={24}
-                    alt=""
-                  />
-                </button>
-                <button
-                  type="button"
-                  className="btn-sm btn btn-outline-danger"
-                >
-                  <img
-                    src="https://cdn-icons-png.flaticon.com/128/1214/1214428.png"
-                    width={24}
-                    height={24}
-                    alt=""
-                  />
-                </button>
-              </td>
-            </tr>
+            {data.map((item, index) => {
+              return (
+                <tr key={index} className="tt">
+                  <th scope="row">{index + 1}</th>
+                  <td>{item.name}</td>
+                  <td>{item.last}</td>
+                  <td>{item.Email}</td>
+                  <td>{item.Phone_Number}</td>
+                  <td>
+                    <button
+                      type="button"
+                      className="btn-sm btn btn-outline-warning"
+                    >
+                      <img
+                        src="https://cdn-icons-png.flaticon.com/128/1159/1159633.png"
+                        width={24}
+                        height={24}
+                        alt=""
+                      />
+                    </button>
+                    <button
+                      type="button"
+                      className="btn-sm btn btn-outline-danger"
+                    >
+                      <img
+                        src="https://cdn-icons-png.flaticon.com/128/1214/1214428.png"
+                        width={24}
+                        height={24}
+                        alt=""
+                      />
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
