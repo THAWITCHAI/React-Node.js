cf2454494b662215f7f776ca2e31ac5899ff8a25
Thawitchai <124795506+THAWITCHAI@users.noreply.github.com>
2024-03-03 01:55:08 +0700

    01

--- DataList/font-back/font-end/src/App.jsx
+++ DataList/font-back/font-end/src/App.jsx
@@ -1,17 +1,7 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Home from "./Component/Contents/Home";
-// import Login from "./Component/Contents/Login";
+
 import MainLayout from "./MainLayout/MainLayout";
 function App() {
   return (
-    // <BrowserRouter>
-    //   <MainLayout />
-    //     <Routes>
-    //       <Route path="/" element={<Home />} />
-    //       <Route path="/" exact={true} element={<Home />} />
-    //       <Route path="/login" element={<Login />} />
-    //     </Routes>
-    // </BrowserRouter>
     <>
       <MainLayout />
     </>
